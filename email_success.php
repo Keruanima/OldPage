b930eb6de5fa014d53783a3491ae9474f1534965
Keruanima <juan.manuel.barcon.lage@gmail.com>
2017-09-22 18:36:49 +0200

    oldpage
    my oldpage

---
+++ email_success.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<meta charset="utf-8">
+<!-- abajo las fonts -->
+<link href='http://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Amaranth:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Jura' rel='stylesheet' type='text/css'>
+<link href='fonts/theleagueof-league-gothic-64c3ede/LeagueGothic-Regular.otf' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+<!-- arriba las fonts -->
+<link rel="stylesheet" type="text/css" href="CSS/style.css" />
+<link rel="stylesheet" href="CSS/fonts.css">
+<link rel="stylesheet" href="CSS/animate.css">
+<!--<link rel="stylesheet" type="text/css" href="CSS/animate.css">-->
+<meta name="viewport" content="width=device-width, user-scalable=no" />
+
+
+</head>
+
+<body>
+<div class="TituloGrande">MENSAJE ENVIADO / EMAIL SUCCESS</div>
+<div class="TituloMediano">Pincha <a href="index.html">AQUÍ</a> para volver a la página.<p>Click <a href="indexEN.html">HERE</a> to return to the main page.</p></div>
+</body>
+</html>
+<!-- arriba el html -->
